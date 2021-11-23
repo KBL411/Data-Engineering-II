5ee0b54da851cf4b49297fd10a0644513eb09d69
Rayan Edjekouane <rayan.edjekouane@efrei.net>
2021-11-23 19:05:16 +0100

    connection

--- app.js
+++ app.js
@@ -14,36 +14,15 @@ const app = express()
 
 app.get('/', function (req, res) {
   db =  mysql.createConnection(mysqlConfig);
+
   db.connect(function(err) {
     if (err) throw err;
     res.send('connected')
   });
+
   db.query('CREATE TABLE IF NOT EXISTS nb_load(id int AUTO_INCREMENT, data INT, log_time DATETIME, PRIMARY KEY(id))')
+
 })
-
-
-app.get('/test', function (req, res) {
-  db.connect(function(err) {
-    if (err) throw err;
-
-    db.query(`SELECT * FROM nb_load`, function (err, result) {
-      if (err) throw err;
-      res.send(JSON.stringify(result))
-    });
-
-    db.query(`INSERT INTO nb_load (data) VALUES (${1})`, function (err) {
-      if (err) throw err;
-      res.send(`new value inserted`)
-    });
-  })
-})
-
-
-
-
-
-
-
 
 app.listen(5000)
 
